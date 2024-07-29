0d5a756cfe672af1c5b748807d6b021a87498964
InnaIvBoiko <inna_boiko@libero.it>
2024-07-29 14:07:57 +0200

    lesson 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import BookList from '../BookList/BookList';
 import Mailbox from '../Mailbox/Mailbox';
 import Product from '../Product/Product';
 import Card from '../Card/Card';
+import Alert from '../Alert/Alert';
 
 //import css from './App.module.css';
 
@@ -26,7 +27,7 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
-      <Product name="Some product" price={0.99}/>
+      <Product name="Some product" price={0.99} />
 
       <h2>Message</h2>
 
@@ -42,6 +43,33 @@ export default function App() {
         <p>Text between opening and closing tag</p>
       </Card>
 
+      <h2>Attribute Style </h2>
+      <p
+        style={{
+          margin: 8,
+          padding: "12px 16px",
+          borderRadius: 4,
+          backgroundColor: "gray",
+          color: "white",
+        }}
+      >
+        Please update your email!
+      </p>
+
+      <h2>Dynamic Stile</h2>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    
     </>
   );
 }
