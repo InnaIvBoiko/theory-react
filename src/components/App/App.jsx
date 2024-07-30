674cf97a5c1923ad416f25232f979baeb7a70816
InnaIvBoiko <inna_boiko@libero.it>
2024-07-30 19:43:22 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,32 @@
-//import css from './App.module.css';
+import css from './App.module.css';
 
 export default function App() {
 
+  // const handelClick = (event) => {
+  //   console.log('Hello world!!!')
+  //   console.log(event)
+  // }
+
+  const handelClick = (massage) => {
+    console.log('Hello world!!!');
+    console.log(massage);
+  };
+
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    console.log('Hello form submit');
+  };
+
   return (
-    <>
-      <h1>Hello world!!!</h1>
-    </>
+    <div className={css.container} >
+      <h1>State in React</h1>
+      <hr />
+      {/* <button onClick={handelClick}>Click me!</button> */}
+      <button onClick={() => handelClick('This is arg')}>Click me!</button>
+
+      <form onSubmit={handelSubmit}>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
